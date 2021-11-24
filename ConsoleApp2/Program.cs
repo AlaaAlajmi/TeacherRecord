4d068e340660b688465210fae74173edc704ee28
AlaaAlajmi <9223@kfh.com>
2021-11-24 12:14:09 +0300

    File Creation Method

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ConsoleApp2
 {
@@ -6,7 +9,33 @@ namespace ConsoleApp2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Create File");
+			FileCreation();
+		}
+		// File Creation Method
+		public static void FileCreation(){
+		// Creating a file
+		string dir = Directory.GetCurrentDirectory();
+		string filename = dir + "TeacherData.txt";
+			//Console.WriteLine("The current directory is {0}", filename);
+
+			// Check if file exists
+			if (File.Exists(filename))
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("*******************************************************");
+				Console.WriteLine("			File Exists");
+				Console.WriteLine("******************************************************* \n");
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("*******************************************************");
+				Console.WriteLine("			File Doesn't Exist");
+				Console.WriteLine("******************************************************* \n");
+				File.CreateText(filename);
+			}
+			List<string> lines = File.ReadAllLines(filename).ToList();
 		}
 	}
 }
